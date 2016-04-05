03e12be345d8fe676780360abb6793e2e7d622a8
Robert Mikhayelyan <rob.mikh@outlook.com>
2016-04-04 22:26:23 -0700

    Updated version number for shared lock feature

--- CompositionImageLoader/Properties/AssemblyInfo.cs
+++ CompositionImageLoader/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.1")]
-[assembly: AssemblyInformationalVersion("0.3.1-alpha")]
-[assembly: AssemblyFileVersion("0.3.1.0")]
+[assembly: AssemblyVersion("0.3.2")]
+[assembly: AssemblyInformationalVersion("0.3.2-alpha")]
+[assembly: AssemblyFileVersion("0.3.2.0")]
 [assembly: ComVisible(false)]
