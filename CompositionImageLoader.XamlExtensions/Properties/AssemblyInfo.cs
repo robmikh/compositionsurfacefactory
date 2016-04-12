205bc4bfc702d2429219cb2ebabfd7ec087d4118
Robert Mikhayelyan <rob.mikh@outlook.com>
2016-04-11 18:55:35 -0700

    Prepping CompositionImageLoader.XamlExtensions for nuget

--- CompositionImageLoader.XamlExtensions/Properties/AssemblyInfo.cs
+++ CompositionImageLoader.XamlExtensions/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CompositionImageLoader.XamlExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("XAML related extensions for CompositionImageLoader.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Robert Mikhayelyan")]
 [assembly: AssemblyProduct("CompositionImageLoader.XamlExtensions")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright �  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: ComVisible(false)]
